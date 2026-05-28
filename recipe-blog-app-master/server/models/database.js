d9e014ff5e06042dac640fff8ac25f45c245328d
Swapnil Urmaliya <swapnilurmaliya794@gmail.com>
2026-05-28 17:52:41 +0530

    fix: add mongoose connection options and error handling for cloud deployment

--- recipe-blog-app-master/server/models/database.js
+++ recipe-blog-app-master/server/models/database.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 10000, // Timeout after 10s if can't connect
+  socketTimeoutMS: 45000,          // Close sockets after 45s of inactivity
+}).catch((err) => {
+  console.error("MongoDB connection error:", err.message);
+  process.exit(1); // Exit so Render knows the app failed to start
+});
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", function () {
   console.log("Connected to DB...");
 });
@@ -14,3 +20,4 @@ db.once("open", function () {
 require("./Category");
 require("./Recipe");
 require("./User");
+
